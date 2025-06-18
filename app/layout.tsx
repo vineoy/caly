fece4d71b1a58684cbff46b77382bc1c2c81aba2
vinayak <singhvinayak288@gmail.com>
2025-06-18 16:20:44 +0530

    feat: Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,6 +71,18 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-E16LLGJHQR"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-E16LLGJHQR');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
